f9a1c4d81d08e71c88a2e16c2e53f4744b22bb4b
Vimlesh kumari <vimlesh3012@gmail.com>
2025-06-24 22:39:15 +0530

    git images added to portfolio

--- vimlesh-portfolio/src/components/contact.js
+++ vimlesh-portfolio/src/components/contact.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 export default function Contact() {
   return (
     <section className="my-8">
       <h2 className="text-2xl font-semibold mb-2">Contact</h2>
       <p>Email: <a href="mailto:vimlesh9512@gmail.com" className="text-blue-500">vimlesh9512@gmail.com</a></p>
-      <img src="">GitHub: <a href="https://github.com/Vimleshkumari" className="text-blue-500">@Vimleshkumari</a></img>
+      <div><span>GitHub:</span> <span><FaGithub /></span><span><a href="https://github.com/Vimleshkumari" className="text-blue-500">@Vimleshkumari</a></span> </div>
     </section>
   );
 }
